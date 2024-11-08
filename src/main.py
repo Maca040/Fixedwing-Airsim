875bc5de52e484f16eeeed95028b3c29cb9afb54
Macarena Vanina Gonzalez <macarena.gonzalez@mi.unc.edu.ar>
2024-11-08 17:49:43 -0300

    Agregado barometro

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+
 from abc import ABC
 import numpy as np
 import airsim
 import gym
+import pprint
 # from tasks import Shaping
 from jsbsim_simulator import Simulation
 from jsbsim_aircraft import Aircraft, cessna172P, ball, x8
@@ -104,6 +106,7 @@ class ClosedLoop:
             if self.display_graphics and graphic_update > graphic_update_old:
                 self.sim.update_airsim()
                 # print('update_airsim')
+
             self.ap.airspeed_hold_w_throttle(self.airspeed)
             self.get_graph_data()
             if not self.over:
@@ -177,8 +180,23 @@ class ClosedLoop:
         self.graph.three_d_scene()
         self.graph.pitch_rate_plot()
         self.graph.roll_rate_plot()
-        # self.graph.roll_rate_plot()
-        # self.debug_aero.get_pitch_values()
+        self.graph.roll_rate_plot()
+        self.debug_aero.get_pitch_values()
+
+   #OBTENER DATOS DEL BARÓMETRO
+
+    client = airsim.MultirotorClient()
+    client.confirmConnection()
+    client.enableApiControl(True)
+
+    state=client.getMultirotorState()
+    s= pprint.pformat(state) 
+    print("state: %s" % s)
+              
+    barometer_data=client.getBarometerData()
+    s = pprint.pformat(barometer_data)
+    print("barometer_data: %s" % s)
+
 
 
 def run_simulator() -> None:
@@ -188,18 +206,23 @@ def run_simulator() -> None:
     :return: None
     """
     env = ClosedLoop(750, True)
-    circuit_profile = ((0, 0, 1000), (4000, 0, 1000), (4000, 4000, 1000), (0, 4000, 1000), (0, 0, 20), (4000, 0, 20),
-                       (4000, 4000, 20))
+    circuit_profile = ((0, 0, 0), (400, 0, 100), (400, 400, 100), (0, 400, 100), (0, 0, 200), (400, 0, 200),
+                       (400, 400, 200), (0, 400, 200), (0, 0, 300), (400, 0, 300),
+                       (400, 400, 300), (0, 400, 300), (0, 0, 400), (400, 0, 400),
+                       (400, 400, 400), (0, 400, 400))
     ice_profile = ((0, 0, 0), (1200, 0, 0), (1300, 150, 0), (540, 530, -80), (0, 0, -150), (100, 100, -100))
-    square = ((0, 0, 0), (2000, 0, 0), (2000, 2000, 0), (0, 2000, 0), (0, 0, 0), (2000, 0, 0), (2000, 2000, 0))
+    square = ((0, 0, 0), (1000, 0, 0), (1000, 1000, 0), (0, 1000, 0), (0, 0, 0), (1000, 0, 0), (1000, 1000, 0))
+    square_mytest = ((0, 0, 0), (1000, 0, 500), (0, 1000, 0), (-1000, 0, 0), (0, -1000, -500))
     approach = ((0, 0, 0), (2000, 0, 800), (2000, 2000, 600), (0, 2000, 400), (0, 0, 200), (2000, 0, 100),
                 (2000, 2000, 100), (0, 2000, 100), (0, 0, 100))
     rectangle = ((0, 0, 0), (2000, 0, 1000), (2000, 2000, 500), (-2000, 2000, 300), (-2000, 0, 100), (2000, 0, 20),
                  (2000, 2000, 20), (-2000, 2000, 20))
     straight = ((0, 0, 0), (2000, 0, 1000), (2000, 2000, 500))
-    env.simulation_loop(straight)
+
+    env.simulation_loop(circuit_profile)
     env.generate_figures()
-    env.report.trace_plot(straight)
+    
+    env.report.trace_plot(circuit_profile)
     env.report.control_response(0, 750, 240)
     env.report.three_d_plot(300, 750, 240)
     print('Simulation ended')
